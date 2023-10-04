3c035ab3651d6f4f00ec89e10eb56283c34623af
007fernando <124810673+007fernando@users.noreply.github.com>
2023-10-04 17:45:45 +0000

    ⭐Feature: Enlazado con checkboxes

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,32 @@
 <script setup>
- import { ref } from 'vue';
-//  const header = ref('🛒 Shopping List App')
+import { ref } from 'vue';
 const header = ref('App Lista de compras');
 const items = ref([
   {id: 1, label: '10 bolillos'},
   {id: 2, label: '1 lata de frijoles'},
   {id: 3, label: '2 lata de atún'}
 ]);
-
-
- //  const items = ref({
-//   "item1": { id: 1, label: '10 bolillos' },
-//   "item2": { id: 2, label: '1 lata de frijoles' },
-//   "item3": { id: 3, label: '2 lata de atún' },
-// });
-
-//  const items = ref([
-//      {id: 1, label: '10 bolillos'},
-//      {id: 2, label: '1 lata de frijoles'},
-//      {id: 3, label: '2 lata de atún'}    
-// ]);
+const newItem= ref('');
+const newItemHighPriority= ref(false);
 </script>
 
 <template>
-   <h1> <i class="material-icons  Shopping-cart-icon"> local_mall</i>  {{ header }}  </h1>
-    <TheWelcome />
-    
+  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
+ <!-- Check Boxes -->
+ <label ><input v-model="newItemHighPriority" type="checkbox">
+  Alta Prioridad
+</label>
+    {{newItemHighPriority ?"🔥": "🧊"}}
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">
       🔹 {{ label }}
     </li>
   </ul>
-   
-   
-    <!-- <ul>
-    <li v-for="({ id, label }, index) in items" v-bind:key="id">
-			🔹 -{{ index }}- {{ label }}
-		</li>
-  </ul> -->
-    
-    
-    <!-- <ul>
-        <li v-for="({ id, label }, index) in items" v-bind:key="id">
-        🔹 - {{ index }} - {{ label }}
-       </li>
-    </ul> -->
-    
-    
-<!--     
-    <ul>
-         <li v-for="{ id, label } in items" v-bind:key="id">🔹 {{ label }}</li>
-       </ul>  
-    
-    <ul>
-         <li v-for="item in items" v-bind:key="item.id">🔹 {{item.label}}</li>
-    </ul> -->
-    
-      <!-- <ul>
-         <li v-for="item in items">🔹 {{item}}</li>
-      </ul> -->
-    <!-- <ul>
-    <li>10 bolillos</li>
-    <li>1 lata de frijoles</li>
-    <li>1 lata de atún</li>
-  </ul> -->
 </template>
 
 <style scoped>
-.Shopping-cart-icon{
-    font-size: 3rem;
+.shopping-cart-icon{
+font-size: 10rem;
 }
 </style>
